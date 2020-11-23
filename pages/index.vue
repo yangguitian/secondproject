73b26a3c2676d2e5b157b1f21fa783f38a81ee18
ygt <1027275314@qq.com>
2020-11-23 17:06:23 +0800

    杨贵添测试第二次

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,7 @@
           rel="noopener noreferrer"
           class="button--green"
         >
-          Documentation111
+          Documentation111123
         </a>
         <a
           href="https://github.com/nuxt/nuxt.js"
